a9cfa7b95deffd4e342f05b3775e61740a4d7a26
meng1992 <762577099@qq.com>
2018-03-18 22:37:06 +0800

    thymeleaf 模板引擎配置和使用

---
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("th")
+public class ThymeleafController {
+
+    @RequestMapping("/index")
+    public String index(ModelMap map){
+        map.addAttribute("name","mqm");
+        return "thymeleaf/index";
+    }
+
+    @RequestMapping("/center")
+    public String center(ModelMap map){
+        return "thymeleaf/center/center";
+    }
+}
