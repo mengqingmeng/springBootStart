faedb04589d2258f238b1ae638582395541e9e5b
mengqingmeng <762577099@qq.com>
2018-03-20 23:04:32 +0800

    thymeleaf

--- src/main/java/com/example/demo/controller/ThymeleafController.java
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("th")
@@ -17,5 +20,17 @@ public class ThymeleafController {
     @RequestMapping("/center")
     public String center(ModelMap map){
         return "thymeleaf/center/center";
+    }
+
+    @RequestMapping("/test")
+    public String test(ModelMap map){
+        User u = new User();
+        u.setAge(13);
+        u.setBirthDay(new Date());
+        u.setDesc("<div>哈哈</div>");
+        u.setName("MQM");
+        u.setPassword("123123");
+        map.put("user",u);
+        return "thymeleaf/test";
     }
 }
