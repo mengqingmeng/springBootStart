51a120dc3f21d8dfe8113033dea8585e17d47b67
mengqingmeng <762577099@qq.com>
2018-03-29 22:20:07 +0800

    整合 mybatis

--- src/main/java/com/example/demo/controller/CURDController.java
+++ src/main/java/com/example/demo/controller/CURDController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.UserService;
+import org.n3r.idworker.Sid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CURDController {
 
     final static Logger log = LoggerFactory.getLogger(CURDController.class);
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    Sid sid;
 
 
 }
