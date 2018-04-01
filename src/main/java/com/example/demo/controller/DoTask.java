7899351b4df6699f4850d51f67d82587a128f17d
mengqingmeng <762577099@qq.com>
2018-04-01 22:02:51 +0800

    interceprot init and configuration

--- src/main/java/com/example/demo/controller/DoTask.java
+++ src/main/java/com/example/demo/controller/DoTask.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.task.AsyncTasks;
+import com.example.demo.utils.JSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ public class DoTask {
     private AsyncTasks asyncTasks;
 
     @RequestMapping("test1")
-    public String test1() throws InterruptedException {
+    public JSONResult test1() throws InterruptedException {
         long start = System.currentTimeMillis();
         Future<Boolean> a = asyncTasks.doTask1();
         Future<Boolean> b = asyncTasks.doTask2();
@@ -28,6 +29,6 @@ public class DoTask {
         long end = System.currentTimeMillis();
         String times ="总共耗时："+(end-start)+"毫秒";
         System.out.println(times);
-        return times;
+        return JSONResult.ok(times);
     }
 }
