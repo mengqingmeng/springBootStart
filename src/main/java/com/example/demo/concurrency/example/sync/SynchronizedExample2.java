e245bb170dee9982850b88808f4c07ddcbe97b83
mengqingmeng <762577099@qq.com>
2018-04-08 22:01:45 +0800

    synchronized修饰静态方法和类：作用对象为给类的所有实例

--- src/main/java/com/example/demo/concurrency/example/sync/SynchronizedExample2.java
+++ src/main/java/com/example/demo/concurrency/example/sync/SynchronizedExample2.java
@@ -6,14 +6,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Slf4j
-public class SynchronizedExample1 {
+public class SynchronizedExample2 {
 
     /**
-     * synchronized修饰静态代码块，锁为调用的对象
+     * synchronized修饰类，作用对象为这个类的所有对象
      */
-    public void test1(int j){
-        //修饰的静态代码块内的代码不执行完成，别的线程不能执行
-        synchronized (this){
+    public static void test1(int j){
+        //因为这里的锁的对象的类，所有该类所有的实例共用一把锁
+        synchronized (SynchronizedExample2.class){
             for (int i=0;i<100;i++){
                 log.info("test1-{}-{}",j,i);
             }
@@ -21,20 +21,20 @@ public class SynchronizedExample1 {
     }
 
     /**
-     * synchronized修饰方法，锁为调用的对象
+     * synchronized修饰静态方法，作用对象为这个类的所有对象
      */
-    public synchronized void test2(int j){
+    public static synchronized void test2(int j){
         for (int i=0;i<100;i++){
             log.info("test2-{}-{}",j,i);
         }
     }
 
     public static void main(String[] args) {
-        SynchronizedExample1 example1 = new SynchronizedExample1();
-        SynchronizedExample1 example2 = new SynchronizedExample1();
+        SynchronizedExample2 example1 = new SynchronizedExample2();
+        SynchronizedExample2 example2 = new SynchronizedExample2();
         ExecutorService executorService = Executors.newCachedThreadPool();
         executorService.execute(()->{
-            example1.test2(1);
+            example2.test2(1);
         });
         executorService.execute(()->{
             example1.test2(2);
