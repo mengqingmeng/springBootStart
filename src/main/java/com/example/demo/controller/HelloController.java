3b74321db01cb4ab51b1a76b3a1bc1177e60c61b
mengqingmeng <762577099@qq.com>
2018-05-09 22:56:28 +0800

    spring security 使用

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,27 +1,27 @@
-package com.example.demo.controller;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Slf4j
-@EnableGlobalMethodSecurity(prePostEnabled = true) //需要加此注解，否则：@PreAuthorize("hasRole('ROLE_ADMIN')")无效
-public class HelloController {
-
-    @RequestMapping("/")
-    public Object index(){
-        log.info("spring boot start");
-        return "spring boot start";
-    }
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @RequestMapping("/hello")
-    public Object hello(){
-        log.info("hello controller");
-        return "hello!";
-    }
-
-}
+//package com.example.demo.controller;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@Slf4j
+//@EnableGlobalMethodSecurity(prePostEnabled = true) //需要加此注解，否则：@PreAuthorize("hasRole('ROLE_ADMIN')")无效
+//public class HelloController {
+//
+//    @RequestMapping("/")
+//    public Object index(){
+//        log.info("spring boot start");
+//        return "spring boot start";
+//    }
+//
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @RequestMapping("/hello")
+//    public Object hello(){
+//        log.info("hello controller");
+//        return "hello!";
+//    }
+//
+//}
