817e3bbbc304b991b816e10df7de94dd092de84c
mengqingmeng <762577099@qq.com>
2018-05-11 17:22:57 +0800

    ·spring security学习

--- src/main/java/com/example/demo/security/MyPasswordEncoder.java
+++ src/main/java/com/example/demo/security/MyPasswordEncoder.java
@@ -1,26 +1,26 @@
-package com.example.demo.security;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-//密码加密
-@Slf4j
-public class MyPasswordEncoder implements PasswordEncoder {
-    @Override
-    public String encode(CharSequence charSequence) {
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        String encode = passwordEncoder.encode(charSequence);
-        log.info("加密后的密码："+encode);
-        return encode;
-    }
-
-    @Override
-    public boolean matches(CharSequence charSequence, String s) {
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        String encode = passwordEncoder.encode(charSequence);
-        boolean match = passwordEncoder.matches(charSequence, encode);
-        log.info("bcrypt密码对比:" + match);
-        return match;
-    }
-}
+//package com.example.demo.security;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+////密码加密
+//@Slf4j
+//public class MyPasswordEncoder implements PasswordEncoder {
+//    @Override
+//    public String encode(CharSequence charSequence) {
+//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        String encode = passwordEncoder.encode(charSequence);
+//        log.info("加密后的密码："+encode);
+//        return encode;
+//    }
+//
+//    @Override
+//    public boolean matches(CharSequence charSequence, String s) {
+//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        String encode = passwordEncoder.encode(charSequence);
+//        boolean match = passwordEncoder.matches(charSequence, encode);
+//        log.info("bcrypt密码对比:" + match);
+//        return match;
+//    }
+//}
