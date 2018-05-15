5d453b792156a4968bb7fda802ccd9ba28c02568
mengqingmeng <762577099@qq.com>
2018-05-15 17:20:33 +0800

    shiro 权限管理 driud 数据源配置

--- src/main/java/com/example/demo/model/init.sql
+++ src/main/java/com/example/demo/model/init.sql
@@ -1,4 +1,4 @@
---权限表--
+-- 权限表 --
 CREATE TABLE permision(
   pid int(11) not null auto_increment,
   name varchar(255) not null default '',
@@ -6,7 +6,12 @@ CREATE TABLE permision(
   primary key (pid)
 ) ENGINE = InnoDB default charset = utf8;
 
---用户表--
+insert into permision values ('1','add','');
+insert into permision values ('2','delete','');
+insert into permision values ('3','edit','');
+insert into permision values ('4','query','');
+
+-- 用户表 --
 create table user(
   uid int(11) not null auto_increment,
   username varchar(255) not null default '',
@@ -14,9 +19,41 @@ create table user(
   primary key (uid)
 ) ENGINE = InnoDB default charset = utf8;
 
---角色表--
+insert into user values ('1','admin','123');
+insert into user values ('2','demo','123');
+
+-- 角色表 --
 create table role(
   rid int(11) not null auto_increment,
   rname varchar(255) not null default '',
   primary key (rid)
 ) ENGINE = InnoDB default charset = utf8;
+
+insert into role values ('1','admin');
+insert into role values ('2','customer');
+
+-- 权限角色关系表 --
+create table permission_role(
+  rid int(11) not null,
+  pid int(11) not null,
+  key idx_rid (rid),
+  key idx_pid (pid)
+)ENGINE = InnoDB default charset = utf8;
+
+insert into permission_role values ('1','1');
+insert into permission_role values ('1','2');
+insert into permission_role values ('1','3');
+insert into permission_role values ('1','4');
+insert into permission_role values ('2','1');
+insert into permission_role values ('2','4');
+
+-- 用户角色关系表 --
+create table user_role(
+  rid int(11) not null,
+  uid int(11) not null,
+  key idx_rid (rid),
+  key idx_uid (uid)
+)ENGINE = InnoDB default charset = utf8;
+
+insert into user_role values (1,1);
+insert into user_role values (2,2);
---
+++ src/main/java/com/example/demo/shiroSecurity/AuthRealm.java
@@ -0,0 +1,61 @@
+package com.example.demo.shiroSecurity;
+
+import com.example.demo.model.Permission;
+import com.example.demo.model.Role;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+//自定义realm
+
+public class AuthRealm extends AuthorizingRealm {
+    @Autowired
+    UserService userService;
+
+    //用于授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        User user = (User) principalCollection.fromRealm(this.getClass().getName()).iterator().next();
+
+        List<String> permissionList = new ArrayList<String>();
+        List<String> roleList = new ArrayList<String>();
+
+        Set<Role> roleSet = user.getRoles();
+        if(!CollectionUtils.isEmpty(roleSet)){
+            for(Role role:roleSet){
+                Set<Permission> permissions = role.getPermissions();
+                roleList.add(role.getRname());
+                if(!CollectionUtils.isEmpty(permissions)){
+                    for(Permission permission:permissions){
+                        String permissionName = permission.getName();
+                        permissionList.add(permissionName);
+                    }
+                }
+            }
+        }
+
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        info.addStringPermissions(permissionList);//将用户拥有的权限存在授权信息中
+        info.addRoles(roleList);    //将用户拥有的角色放在授权信息中
+        return info;
+    }
+
+    //用于认证登陆
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        String userName = token.getUsername();
+        User user = userService.findByUserName(userName);
+        return new SimpleAuthenticationInfo(user,user.getPassword(),this.getClass().getName());
+    }
+}
